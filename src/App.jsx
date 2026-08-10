1a5dabb2671a83e3a7e4439120d820ab6d7a1890
Isac <contatoisacn@gmail.com>
2026-08-10 09:55:22 -0300

    feat: useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from 'react'
-import './App.css'
+import Botao from './components/Botao'
+import Aluno from "./components/Aluno"
+import Dados from "./components/Dados"
+import Contador from "./components/Contador"
 
 function App() {
 
+  const enviar=()=>{
+    alert("Botão sem componente")
+  }
+
   return (
   <>
+    <Botao/>
+    <Aluno nome="Fiap" idade="30"/>
+    <Dados usuario="Huguinho" email="huguinho@email.com"/>
+    <button onClick={enviar}>Botão sem componente</button>
+    <Contador/>
   </>
   )
   
---
+++ src/components/Contador.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+// Importando o Hook useState
+import {useState} from "react"
+
+const Contador = () => {
+
+    // HOOK - useState - Manipula o estadp da variável
+    const [contador,setContador]=useState(0);
+
+  return (
+    <>
+      <h1>Contagem inicial:{contador}</h1>
+      <button onClick={()=>setContador(contador+1)}>Aumentar</button>
+      <button onClick={()=>setContador(contador-1)}>Diminuir</button>
+    </>
+  )
+}
+
+export default Contador
